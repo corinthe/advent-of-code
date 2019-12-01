29ba09a0e589c02bba356e0f17760a1ce8012384
arassart <antoinerassart@hotmail.be>
2019-12-01 18:36:51 +0100

    Solution day 2019.1

--- src/main/scala/com/lmat/adventofcode/year2019/Day01.scala
+++ src/main/scala/com/lmat/adventofcode/year2019/Day01.scala
@@ -10,22 +10,22 @@ object Day01 extends SimpleCommonPuzzle[Seq[Int], Int, Int] {
   override def parse(resource: String): Seq[Int] =
     readResource(resource).flatMap(row => Try(row.toInt).toOption)
 
-  override def part1(moduleMasses: Seq[Int]): Int =
-    moduleMasses.map(fuel).sum
+  override def part1(masses: Seq[Int]): Int =
+    masses.map(mass => fuel(mass)).sum
 
-  override def part2(moduleMasses: Seq[Int]): Int =
-    moduleMasses.map(fuel2).sum
+  override def part2(masses: Seq[Int]): Int =
+    masses.map(mass => totalMass(mass)).sum
+
+  def totalMass(initialMass: Int): Int = {
+    @tailrec
+    def iterate(remaining: Int, total: Int) : Int = remaining match {
+      case remaining if remaining <= 0 => total
+      case remaining => iterate(fuel(remaining), remaining + total)
+    }
+
+    iterate(fuel(initialMass), 0)
+  }
 
   def fuel(mass: Int): Int = (mass / 3) - 2
 
-  def fuel2(mass: Int): Int = {
-    @tailrec
-    def iterate(total: Int, current: Int): Int =
-      if(current == 0) total
-      else {
-        val f = fuel(current) max 0
-        iterate(total + f, f)
-      }
-    iterate(0, mass)
-  }
 }
