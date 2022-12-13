07d476fc74d087e3bfed621402b6019237208f6c
arassart <antoine.rassart@5thfloor.be>
2022-12-13 13:36:40 +0100

    2022.12

---
+++ src/main/scala/com/lmat/adventofcode/year2022/Day12.scala
@@ -0,0 +1,21 @@
+package com.lmat.adventofcode.year2022
+
+import com.lmat.adventofcode.SimpleCommonPuzzle
+import com.lmat.util.Files.readResource
+import com.lmat.util.Matrix
+
+object Day12Definitions {
+
+
+
+}
+
+object Day12 extends SimpleCommonPuzzle[Seq[String], Int, Int] {
+
+  override def parse(resource: String): Seq[String] =
+    readResource(resource)
+
+  override def part1(input: Seq[String]): Int = ???
+
+  override def part2(input: Seq[String]): Int = ???
+}
